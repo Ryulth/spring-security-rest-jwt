94d645e5f5ff0824c3d6449f4f963261dd5b8841
jeju <jeju@hpcnt.com>
2020-01-01 10:45:57 +0900

    add swagger

--- src/main/java/com/ryulth/springsecurityrestjwt/config/WebSecurityConfig.java
+++ src/main/java/com/ryulth/springsecurityrestjwt/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.ryulth.springsecurityrestjwt.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,97 +29,118 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private static final RequestMatcher API_URLS = new OrRequestMatcher(
-            new AntPathRequestMatcher("/auth/**")
-    );
-    private static final RequestMatcher PROTECTED_URLS = new NegatedRequestMatcher(API_URLS);
 
-    private static final String[] SWAGGER_URLS = {
-            "/v2/api-docs/**",
-            "/configuration/ui/**",
-            "/swagger-resources/**",
-            "/configuration/security/**",
-            "/swagger-ui.html/**",
-            "/webjars/**"
-    };
+    @Order(1)
+    @Configuration
+    public static class JWTRestSecurityConfig extends WebSecurityConfigurerAdapter {
+        private static final RequestMatcher PROTECTED_URLS = new OrRequestMatcher(
+                new AntPathRequestMatcher("/api/**")
+        );
 
-    JWTTokenAuthenticationProvider provider;
+        JWTTokenAuthenticationProvider provider;
 
-    WebSecurityConfig(final JWTTokenAuthenticationProvider provider) {
-        super();
-        this.provider = requireNonNull(provider);
+        JWTRestSecurityConfig(JWTTokenAuthenticationProvider provider) {
+            super();
+            this.provider = requireNonNull(provider);
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.authenticationProvider(provider);
+        }
+
+        @Override
+        public void configure(WebSecurity web) {
+            web.ignoring().mvcMatchers(HttpMethod.OPTIONS, "/**");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .sessionManagement()
+                    .sessionCreationPolicy(STATELESS)
+                    .and()
+                    .exceptionHandling()
+                    // this entry point handles when you request a protected page and you are not yet
+                    // authenticated
+                    .defaultAuthenticationEntryPointFor(forbiddenEntryPoint(), PROTECTED_URLS)
+                    .and()
+                    .authenticationProvider(provider)
+                    .addFilterBefore(restAuthenticationFilter(), AnonymousAuthenticationFilter.class)
+                    .authorizeRequests()
+                    .requestMatchers(PROTECTED_URLS)
+                    .authenticated()
+                    .and()
+                    .csrf().disable()
+                    .formLogin().disable()
+                    .httpBasic().disable()
+                    .logout().disable();
+
+        }
+
+        @Bean
+        TokenAuthenticationFilter restAuthenticationFilter() throws Exception {
+            final TokenAuthenticationFilter filter = new TokenAuthenticationFilter(PROTECTED_URLS);
+            filter.setAuthenticationManager(authenticationManager());
+            filter.setAuthenticationSuccessHandler(successHandler());
+            return filter;
+        }
+
+        @Bean
+        SimpleUrlAuthenticationSuccessHandler successHandler() {
+            final SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
+            successHandler.setRedirectStrategy(new NoRedirectStrategy());
+            return successHandler;
+        }
+
+        /**
+         * Disable Spring boot automatic filter registration.
+         */
+        @Bean
+        FilterRegistrationBean disableAutoRegistration(final TokenAuthenticationFilter filter) {
+            final FilterRegistrationBean registration = new FilterRegistrationBean(filter);
+            registration.setEnabled(false);
+            return registration;
+        }
+
+        @Bean
+        AuthenticationEntryPoint forbiddenEntryPoint() {
+            return new HttpStatusEntryPoint(FORBIDDEN);
+        }
     }
 
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(provider);
-        auth.inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("1234")).roles("SWAGGER");
+    @Order(2)
+    @Configuration
+    public static class SwaggerSecurityConfig extends WebSecurityConfigurerAdapter {
+        private static final RequestMatcher SWAGGER_URLS = new OrRequestMatcher(
+                new AntPathRequestMatcher("/v2/api-docs")
+        );
+        private static final String SWAGGER = "SWAGGER";
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .withUser("admin").password(passwordEncoder().encode("1234")).roles(SWAGGER);
+        }
+        @Override
+        public void configure(WebSecurity web) {
+            web.ignoring().mvcMatchers(HttpMethod.OPTIONS, "/**");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .requestMatchers(SWAGGER_URLS).hasRole(SWAGGER)
+                    .and()
+                    .csrf().disable()
+                    .httpBasic();
+        }
+
+        @Bean
+        PasswordEncoder passwordEncoder() {
+            return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        }
     }
 
-    @Override
-    public void configure(final WebSecurity web) {
-        web.ignoring().requestMatchers(API_URLS);
-        web.ignoring().mvcMatchers(HttpMethod.OPTIONS, "/**");
-        web.ignoring().antMatchers(SWAGGER_URLS);
-    }
-
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
-                .sessionManagement()
-                .sessionCreationPolicy(STATELESS)
-                .and()
-                .exceptionHandling()
-                // this entry point handles when you request a protected page and you are not yet
-                // authenticated
-                .defaultAuthenticationEntryPointFor(forbiddenEntryPoint(), PROTECTED_URLS)
-                .and()
-                .authenticationProvider(provider)
-                .addFilterBefore(restAuthenticationFilter(), AnonymousAuthenticationFilter.class)
-                .authorizeRequests()
-                .requestMatchers(PROTECTED_URLS)
-                .authenticated()
-                .and()
-                .csrf().disable()
-                .formLogin().disable()
-                .httpBasic().disable()
-                .logout().disable();
-
-    }
-
-    @Bean
-    TokenAuthenticationFilter restAuthenticationFilter() throws Exception {
-        final TokenAuthenticationFilter filter = new TokenAuthenticationFilter(PROTECTED_URLS);
-        filter.setAuthenticationManager(authenticationManager());
-        filter.setAuthenticationSuccessHandler(successHandler());
-        return filter;
-    }
-
-    @Bean
-    SimpleUrlAuthenticationSuccessHandler successHandler() {
-        final SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
-        successHandler.setRedirectStrategy(new NoRedirectStrategy());
-        return successHandler;
-    }
-
-    /**
-     * Disable Spring boot automatic filter registration.
-     */
-    @Bean
-    FilterRegistrationBean disableAutoRegistration(final TokenAuthenticationFilter filter) {
-        final FilterRegistrationBean registration = new FilterRegistrationBean(filter);
-        registration.setEnabled(false);
-        return registration;
-    }
-
-    @Bean
-    AuthenticationEntryPoint forbiddenEntryPoint() {
-        return new HttpStatusEntryPoint(FORBIDDEN);
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
 }
